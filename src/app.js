2bc4504b13f7f7a931bf436b079f34c69a7e6d8b
Luke Brobbin <lukemichaelvocalist@gmail.com>
2022-08-08 15:40:11 +0100

    chore: add morgan

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 const express = require("express");
+const morgan = require("morgan");
 const jokesRouter = require("./routes/jokes");
 const app = express();
 
 app.use(express.json());
 app.use(express.static("public"));
+app.use(morgan("tiny"));
 
 app.use("/jokes", jokesRouter);
 
